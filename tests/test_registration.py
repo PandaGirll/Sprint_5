a2a98f564d715f0fbf8afb71da40c7c6c177096f
PandaGirll <nikaklimenko@gmail.com>
2024-05-29 02:43:58 +0300

    Тест перехода в личный кабинет, удаление лишних импортов

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,6 +1,4 @@
-import pytest
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from locators import TestLocators
 from data import TestLinks, AuthHelper
