dafc3138d478b5f6a54906097be3f8b2eee9e00a
PandaGirll <nikaklimenko@gmail.com>
2024-06-01 22:40:59 +0300

    Функция универсальной авторизации из файла data.py вынесена в отдельный файл auth_helper.py и разделена на независимые методы. Исправлен test_registration.py, старый метод регистрации заменён на новый. Код приведён к соответствию стандарту PEP8.

--- auth_helper.py
+++ auth_helper.py
@@ -8,7 +8,6 @@ from locators import TestLocators
 
 
 class AuthHelper:
-
     # Метод для логина пользователя
     @staticmethod
     def login(driver, email, password):
@@ -19,40 +18,44 @@ class AuthHelper:
         driver.find_element(*TestLocators.PASSWORD_LOCATOR).send_keys(password)
         # Нажимаем кнопку "Войти"
         driver.find_element(*TestLocators.LOGIN_BUTTON_LOCATOR).click()
-        # Проверяем, что логин был успешен
-        AuthHelper.confirm_login_success(driver)
 
+    # Метод для регистрации пользователя
     @staticmethod
     def registration(driver, email, password, name="Тест"):
-        # Заполняем форму регистрации
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-            TestLocators.NAME_LOCATOR))
-        driver.find_element(*TestLocators.NAME_LOCATOR).send_keys(name)
-        driver.find_element(*TestLocators.EMAIL_LOCATOR).send_keys(email)
-        driver.find_element(*TestLocators.PASSWORD_LOCATOR).send_keys(password)
-        # Нажимаем кнопку "Зарегистрироваться"
-        driver.find_element(*TestLocators.REGISTER_BUTTON_LOCATOR).click()
-        # Проверка, что пользователь уже существует
-        if AuthHelper.is_user_already_exists(driver):
-            # Генерируем новый email, и# обновляем страницу
-            email = Generator.generate_email()
-            driver.refresh()  # Обновляем страницу для повторной регистрации
-            # Регистрируемся заново с новым email
-            AuthHelper.registration(driver, email, password, name)
-        else:
-            # Проверяем, что регистрация была успешна
-            AuthHelper.confirm_registration_success(driver)
+        def fill_registration_form(driver, email, password, name):
+            # Заполняем форму регистрации
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located(TestLocators.NAME_LOCATOR))
+            driver.find_element(*TestLocators.NAME_LOCATOR).send_keys(name)
+            driver.find_element(*TestLocators.EMAIL_LOCATOR).send_keys(email)
+            driver.find_element(
+                *TestLocators.PASSWORD_LOCATOR).send_keys(password)
+            # Нажимаем кнопку "Зарегистрироваться"
+            driver.find_element(*TestLocators.REGISTER_BUTTON_LOCATOR).click()
 
+        fill_registration_form(driver, email, password, name)
+
+        # Проверка, что пользователь уже существует
+        while AuthHelper.is_user_already_exists(driver):
+            email = Generator.generate_email()  # Генерируем новый email
+            driver.refresh()  # Обновляем страницу
+            fill_registration_form(driver, email, password, name)
+
+    # Проверка успешности редиректа после логина
     @staticmethod
     def confirm_login_success(driver):
         # Проверяем, что мы на главной странице
-        WebDriverWait(driver, 10).until(EC.url_to_be(TestLinks.main_page_link))
+        WebDriverWait(driver, 10).until(EC.url_to_be(
+            TestLinks.main_page_link))
 
+    # Проверка успешности редиректа после регистрации
     @staticmethod
     def confirm_registration_success(driver):
         # Проверяем, что мы на странице логина
-        WebDriverWait(driver, 10).until(EC.url_to_be(TestLinks.login_page_link))
+        WebDriverWait(driver, 10).until(EC.url_to_be(
+            TestLinks.login_page_link))
 
+    # Проверка на появление ошибки после регистрации
     @staticmethod
     def is_user_already_exists(driver):
         # Проверка появления ошибки "Такой пользователь уже существует"
--- data.py
+++ data.py
@@ -1,11 +1,3 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-from generator import Generator
-from locators import TestLocators
-
-
 # Ссылки на страницы
 class TestLinks:
     testing_url = "https://stellarburgers.nomoreparties.site"  # Адрес тестируемого сервера
@@ -17,38 +9,4 @@ class TestLinks:
     personal_account_page_link = testing_url + "/account/profile"
 
 
-# Универсальная функция для регистрации и авторизации
-class AuthHelper2:
-    @staticmethod
-    def authenticate(driver, email, password, is_registration=False):
-        while True:
-            if is_registration:
-                # Предполагаем, что страница регистрации уже открыта
-                driver.find_element(*TestLocators.NAME_LOCATOR).send_keys("Тест")
 
-            # Заполняем форму входа или регистрации
-            WebDriverWait(driver, 5).until(EC.presence_of_element_located(TestLocators.EMAIL_LOCATOR))
-            driver.find_element(*TestLocators.EMAIL_LOCATOR).send_keys(email)
-            driver.find_element(*TestLocators.PASSWORD_LOCATOR).send_keys(password)
-
-            # Нажимаем кнопку «Войти» или «Зарегистрироваться» соответственно.
-            driver.find_element(
-                *TestLocators.LOGIN_BUTTON_LOCATOR if not is_registration else TestLocators.REGISTER_BUTTON_LOCATOR).click()
-
-            if is_registration:
-                # Проверка появления ошибки "Такой пользователь уже существует"
-                try:
-                    WebDriverWait(driver, 3).until(
-                        EC.visibility_of_element_located(TestLocators.ERROR_MESSAGE_LOCATOR)
-                    )
-                    # Если ошибка появилась, генерируем новый email
-                    email = Generator.generate_email()
-                    driver.refresh()  # Обновляем страницу для повторной регистрации
-                except TimeoutException:
-                    break  # Если ошибка не появилась, выходим из цикла
-            else:
-                break
-
-        # После логина проверяем, что мы на главной странице.
-        expected_url_substring = "/login" if is_registration else "/"
-        WebDriverWait(driver, 10).until(EC.url_contains(expected_url_substring))
