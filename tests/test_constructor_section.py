878a66f1758239e38709166eb4466f3ca0b123de
PandaGirll <nikaklimenko@gmail.com>
2024-06-01 23:37:41 +0300

    Заменены Локаторы для тестов конструктора. Исправлен test_constructor_section.py, в тестах перехода по вкладкам используются новые локаторы класса.
    Код приведён к соответствию стандарту PEP8.

--- tests/test_constructor_section.py
+++ tests/test_constructor_section.py
@@ -7,90 +7,57 @@ from locators import TestLocators
 
 # Содаём класс для проверки переходов к разделам конструктора
 class TestNavigationInConstructor:
-    # Проверяем переход в разделы конструктора по клику на соответствующие вкладки
-
+    # Проверяем переход в разделы конструктора по клику на вкладки
     # Вкладка Булки изначально выбрана и некликабельна
     def test_navigation_to_buns(self, driver, test_email, test_password):
         # Переход на главную страницу
         driver.get(TestLinks.main_page_link)
+        # Клик по вкладке «Соусы» чтобы уйти с вкладки "Булки"
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+            TestLocators.CONSTRUCTOR_TAB_SAUCE_LOCATOR)).click()
+        # Клик по вкладке «Булки»
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+            TestLocators.CONSTRUCTOR_TAB_BUN_LOCATOR)).click()
+        # Проверка, что вкладка "Булки" стала активной
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+            TestLocators.ACTIVE_TAB_BUN_LOCATOR))
+        # Определение ожидаемого и фактического результата
+        buns_tab = driver.find_element(*TestLocators.ACTIVE_TAB_BUN_LOCATOR)
+        expected_class = 'tab_tab_type_current__2BEPc'
+        actual_class = buns_tab.get_attribute('class')
+        assert expected_class in actual_class, "Вкладка 'Булки' не активна."
 
-        # Прокрутка к разделу «Булки»
-        buns_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.BUNS_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", buns_section)
-
-        # Проверка, что раздел «Булки» виден в области просмотра
-        assert buns_section.is_displayed(), "Ошибка отображения раздела «Булки»."
-
-    def test_navigation_to_sauces(self, driver, test_email, test_password):
+    def test_navigation_to_sauces(self, driver):
         # Переход на главную страницу
         driver.get(TestLinks.main_page_link)
-
-        # Клик по вкладке «Соусы» и прокрутка к разделу «Соусы»
-        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.CONSTRUCTOR_TAB_SAUCE_LOCATOR)).click()
-        sauces_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.SAUCES_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", sauces_section)
-
-        # Проверка, что раздел «Соусы» виден в области просмотра
-        assert sauces_section.is_displayed(), "Ошибка отображения раздела «Соусы»."
+        # Клик по вкладке «Соусы»
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+            TestLocators.CONSTRUCTOR_TAB_SAUCE_LOCATOR)).click()
+        # Проверка, что вкладка "Соусы" стала активной
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                TestLocators.ACTIVE_TAB_SAUCE_LOCATOR))
+        # Определение ожидаемого и фактического результата
+        sauces_tab = driver.find_element(
+            *TestLocators.ACTIVE_TAB_SAUCE_LOCATOR)
+        expected_class = 'tab_tab_type_current__2BEPc'
+        actual_class = sauces_tab.get_attribute('class')
+        assert expected_class in actual_class, "Вкладка 'Соусы' не активна."
 
     def test_navigation_to_fillings(self, driver, test_email, test_password):
         # Переход на главную страницу
         driver.get(TestLinks.main_page_link)
 
-        # Клик по вкладке «Начинки» и прокрутка к разделу «Начинки»
-        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.CONSTRUCTOR_TAB_FILLING_LOCATOR)).click()
-        fillings_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.FILLINGS_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", fillings_section)
-
-        # Проверка, что раздел «Начинки» виден в области просмотра
-        assert fillings_section.is_displayed(), "Ошибка отображения раздела «Начинки»."
-
-    # Альтернативный вариант проверки навигации по клику на вкладки, булки в конце, чтобы были кликабельны
-
-    def test_navigation_to_sauces_section(self, driver, test_email, test_password):
-        # Переход на главную страницу
-        driver.get(TestLinks.main_page_link)
-
-        # Клик по вкладке «Соусы»
-        sauces_tab = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable(TestLocators.CONSTRUCTOR_TAB_SAUCE_LOCATOR))
-        driver.execute_script("arguments[0].click();", sauces_tab)
-
-        # Проверка, что отображается подраздел «Соусы»
-        sauces_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.SAUCES_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", sauces_section)
-        assert sauces_section.is_displayed(), "Ошибка отображения раздела «Соусы»."
-
-    def test_navigation_to_fillings_section(self, driver, test_email, test_password):
-        # Переход на главную страницу
-        driver.get(TestLinks.main_page_link)
-
         # Клик по вкладке «Начинки»
-        fillings_tab = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable(TestLocators.CONSTRUCTOR_TAB_FILLING_LOCATOR))
-        driver.execute_script("arguments[0].click();", fillings_tab)
-
-        # Проверка, что отображается подраздел «Начинки»
-        fillings_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.FILLINGS_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", fillings_section)
-        assert fillings_section.is_displayed(), "Ошибка отображения раздела «Начинки»."
-
-    def test_navigation_to_buns_section(self, driver, test_email, test_password):
-        # Переход на главную страницу
-        driver.get(TestLinks.main_page_link)
-
-        # Клик по вкладке «Булки»
-        buns_tab = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable(TestLocators.CONSTRUCTOR_TAB_BUN_LOCATOR))
-        driver.execute_script("arguments[0].click();", buns_tab)
-
-        # Проверка, что отображается подраздел «Булки»
-        buns_section = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located(TestLocators.BUNS_SECTION_LOCATOR))
-        driver.execute_script("arguments[0].scrollIntoView(true);", buns_section)
-        assert buns_section.is_displayed(), "Ошибка отображения раздела «Булки»."
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+            TestLocators.CONSTRUCTOR_TAB_FILLING_LOCATOR)).click()
+        # Проверка, что вкладка "Начинки" стала активной
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                TestLocators.ACTIVE_TAB_FILLING_LOCATOR))
+        # Определение ожидаемого и фактического результата
+        fillings_tab = driver.find_element(
+            *TestLocators.ACTIVE_TAB_FILLING_LOCATOR)
+        expected_class = 'tab_tab_type_current__2BEPc'
+        actual_class = fillings_tab.get_attribute('class')
+        assert expected_class in actual_class, "Вкладка 'Начинки' не активна."
